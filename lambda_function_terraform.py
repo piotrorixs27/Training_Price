6988c5ccb2558c5be9fdc8ad8f3e1c09f517da05
piotrorixs27 <135148069+piotrorixs27@users.noreply.github.com>
2023-07-08 13:14:53 +0200

    Delete lambda_function_terraform.py

--- lambda_function_terraform.py
+++
@@ -1,7 +0,0 @@
-import boto3
-dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('Table_Price_Training_Terraform')
-def lambda_handler(event, context):
-      response=table.scan()
-      return response["Items"]
-      
